aa03789a428cf391277caea327ddfe980f2b94c0
JosephVentrella <ventrejo@oregonstate.edu>
2022-05-09 23:48:25 -0700

    Remove all similar case statements Remove the now similar case statements in line method and replace them with a single line

--- lib/house.rb
+++ lib/house.rb
@@ -1,33 +1,6 @@
 class House
     def line(number)
-        case number
-        when 1
-            "This is the #{phrase(number)}\n"
-        when 2
-            "This is the #{phrase(number)}\n"
-        when 3
-            "This is the #{phrase(number)}\n"
-        when 4
-            "This is the #{phrase(number)}\n"
-        when 5
-            "This is the #{phrase(number)}\n"
-        when 6
-            "This is the #{phrase(number)}\n"
-        when 7
-            "This is the #{phrase(number)}\n"
-        when 8
-            "This is the #{phrase(number)}\n"
-        when 9
-            "This is the #{phrase(number)}\n"
-        when 10
-            "This is the #{phrase(number)}\n"
-        # when 11
-        #     "This is the #{phrase(number)}\n"
-        # when 12
-        #     "This is the #{phrase(number)}\n"
-        else
-            "This is the #{phrase(number)}\n"
-        end
+        "This is the #{phrase(number)}\n"
     end
     def recite()
         (1..12).map{|n| line(n)}.join("\n")
