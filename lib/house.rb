af32e2c317310803812c77e8c5f5ffc6d42b76b8
JosephVentrella <ventrejo@oregonstate.edu>
2022-06-03 03:43:40 -0700

    Extract the dog line into phrase method

--- lib/house.rb
+++ lib/house.rb
@@ -10,9 +10,9 @@ class House
         when 4
             "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 5
-            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is #{phrase(number -7 )}the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
-            "This is #{phrase(number)}#{phrase(number -1 )}#{phrase(number -2 )}#{phrase(number -3 )}#{phrase(number -4 )}#{phrase(number -5 )}#{phrase(number -6 )}the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is #{phrase(number)}#{phrase(number -1 )}#{phrase(number -2 )}#{phrase(number -3 )}#{phrase(number -4 )}#{phrase(number -5 )}#{phrase(number -6 )}#{phrase(number -7 )} the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
     def recite()
@@ -35,6 +35,8 @@ class House
             "the maiden all forlorn that milked "
         when 6
             "the cow with the crumpled horn that tossed "
+        when 5
+            "the dog that worried "
         else
             ""
         end
